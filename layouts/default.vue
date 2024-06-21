645e637d0531127c227e5c12a84c25544f03517a
Uddhav Paudel <devuddhav15@gmail.com>
2024-06-21 20:23:00 +0100

    1.0.2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const drawer = ref<boolean>(false);
+</script>
 
 <template>
 	<div>
-		Layout: default
-		<slot />
+		<v-app>
+			<v-toolbar app>
+				<v-navigation-drawer app v-if="drawer"> </v-navigation-drawer>
+				<v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+				<v-toolbar-title> Paude Udi </v-toolbar-title>
+			</v-toolbar>
+			<v-main>
+				<NuxtPage> </NuxtPage>
+			</v-main>
+		</v-app>
 	</div>
 </template>
 
