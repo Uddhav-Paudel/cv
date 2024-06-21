1243800f61a2f17150a0e9a0b9f5d8c780010e22
Uddhav Paudel <devuddhav15@gmail.com>
2024-06-21 20:49:08 +0100

    1.0.5
    Cleanup.

--- app.vue
+++ app.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 const drawer = ref<boolean>(false);
-const layout = "default";
 </script>
 <template>
 	<div>
-		<!-- <NuxtLayout :name="layout"> -->
 		<v-toolbar>
 			<v-navigation-drawer app v-if="drawer"> </v-navigation-drawer>
 			<v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
 			<v-toolbar-title> Paude Udi </v-toolbar-title>
 		</v-toolbar>
 		<NuxtPage />
-		<!-- </NuxtLayout> -->
 	</div>
 </template>
