6a6f8705cdd3e36afd41facd30ecf833b9e959e1
Uddhav Paudel <devuddhav15@gmail.com>
2025-01-12 21:09:06 +0000

    Test

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,18 @@
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "C:/Users/UR-PC/Documents/Udi/GitHub/up.cv/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "C:/Users/UR-PC/Documents/Udi/GitHub/up.cv/node_modules/nuxt/dist/app/entry.js"
+  }
+}
---
+++ dist/_nuxt/DCLzFe7G.js
@@ -0,0 +1,1 @@
+import{w as t}from"./Dqp31ZR5.js";const i=t("v-list-item-title");export{i as V};
